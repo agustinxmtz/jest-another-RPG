57026cb1b25625c9e27a674952b5eb11eb70ba7b
agustinxmtz <70080354+agustinxmtz@users.noreply.github.com>
2021-02-19 22:27:40 -0600

    Test that will fail Potion Test

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,12 @@
+const Potion = require('../lib/Potion');
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+
+
